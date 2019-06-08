4dfb58b9a3fdcb84ffe364176e537ccf1b2c15b1
dbcerda <dbcerda@inf.ufsm.br>
2019-06-08 17:41:18 -0300

    version 0.1

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ BOT_PREFIX = os.environ['prefix']
 TOKEN = os.environ['token']
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
-# TODO implementar mostrar fila e ggez
-# TODO (modo de fila rapida ou normal (FT2 ou FT3) conforme tamanho da fila)
-# TODO (avisar quando jogador fez volta inteira na fila)
-# TODO limite de rodadas
+# TODO winstreak - integracao com twitch?
+# TODO estatisticas (longest winstreak, maior numero de vitorias, lista de quem cada jogador venceu)
+# TODO avisar quando jogador fez volta inteira na fila
+# TODO guardar IP dos jogadores e passar no call_next_match()
+# TODO limite de rodadas por evento
 # TODO caixa de bombom
-# TODO streak functionality - integracao com twitch?
+# TODO funcao para reverter o ultimo vencedor caso haja algum troll/equivoco
+# TODO lista negra de usuarios que trollam
 
 BOMBONS = ["Prestigio", "Alpino", "Suflair", "Classic", "Galak", "Lollo", "Charge", "Sensação", "Chokito", "Negresco", "Smash"]
 
@@ -28,44 +30,39 @@ class QManager:
 
     # -- DATA HANDLING FUNCTIONS --
 
-    # returns relative position of entering player or the position he is already in
-    # TODO resolve async dependencies
-    def add_player(self, context):
+    async def add_player(self, context):
         member = context.author
         if member in self.queue:
-            return - (self.queue.index(member) + 1)
-        self.queue.append(member)
-        if self.active_queue():
-            return self.queue.index(member) - 1
+            await context.send(f'{member.name} ja esta na fila na posicao {self.queue.index(member) + 1}')
         else:
-            if len(self.queue) == 2:
-                self.call_next_match(context)
-            return 0
+            self.queue.append(member)
+            if self.number_of_players() == 2:
+                await self.call_next_match(context)
+            elif self.active_queue():
+                await context.send(f"{member.name} foi adicionado a fila na posicao {self.queue.index(member) - 1}")
+            else:
+                await context.send(f"{member.name} esta esperando desafiantes")
 
-    def remove_player(self, context):
+    async def remove_player(self, context):
         member = context.author
         if member not in self.queue:
-            return False
+            await context.send(f"{member.name} nao esta na fila")
         else:
             player_pos = self.queue.index(member)
             self.queue.remove(member)
-            if player_pos < 2 and len(self.queue) > 1:
-                self.call_next_match(context)
-        return True
+            await context.send(f"{member.name} correu que nem um franguinho")
+            if player_pos < 2 and self.number_of_players() > 1:
+                await self.call_next_match(context)
 
-    def skip_turn(self, context):
+    async def skip_turn(self, context):
         member = context.author
-        if member not in self.queue[:2] or not self.active_queue():
-            return False
-        else:
-            if member not in self.queue[:2]:
-                self.queue.remove(member)
-                self.queue.append(member)
-            else:
-                self.queue.remove(member)
-                self.queue.append(member)
-                self.call_next_match(context)
-            return True
+        if member not in self.queue or not self.active_queue():
+            await context.send(f"Nao")
+        elif member in self.first_players():
+            self.queue.remove(member)
+            self.queue.append(member)
+            await self.call_next_match(context)
+            await context.send(f"{member.name} foi pro fim da fila")
 
     def force_skip(self, context, target):
         self.queue.remove(target)
@@ -79,12 +76,9 @@ class QManager:
         # TODO se admin precisar remover um usuario
         return
 
-    def resolve_match(self, context):
+    async def resolve_match(self, context):
         winner = context.author
-        if len(self.queue) < 3:
-            return
-        active_players = self.queue[:2]
-        if winner in active_players:
+        if winner in self.first_players():
             self.last_winner = winner
             pos = self.queue.index(winner)
             if pos == 1:  # winner is second in line
@@ -93,19 +87,18 @@ class QManager:
                 loser = self.queue.pop(1)
 
             self.queue.append(loser)
-            self.call_next_match(context)
+            await self.call_next_match(context)
             # TODO statistics
-            return
         else:
-            return
+            await context.send(f"Nao mente, rapaz!")
 
-    def active_players(self):
+    def first_players(self):
         return self.queue[0:2]
 
     def show_queue(self):
         return [member.name for member in self.queue]
 
-    def player_count(self):
+    def number_of_players(self):
         return len(self.queue)
 
     def active_queue(self):
@@ -114,8 +107,14 @@ class QManager:
     def reset(self):
         pass  # TODO resetar dados
 
-    def call_next_match(self, context):
-        context.send(f'Proxima partida: {self.queue[0].mention} vs {self.queue[1].mention}')
+    async def call_next_match(self, context):
+        await context.send(f'Proxima partida: {self.queue[0].mention} vs {self.queue[1].mention} - {self.rule_set()}')
+
+    def rule_set(self):
+        if len(self.queue) > 4:
+            return "FT2"
+        else:
+            return "FT3"
 
 
 QUEUE = QManager()
@@ -157,37 +156,25 @@ async def close_queue(ctx):
 @bot.command(name="entrar")
 # @is_channel("eventos")
 async def join_queue(ctx):
-    value = QUEUE.add_player(ctx)
-    if value > 0:
-        await ctx.send("{}  foi adicionado a fila na posicao {}".format(ctx.author.name,value))
-    elif value < 0:
-        await ctx.send("{} ja esta na fila na posicao {}".format(ctx.author.name, abs(value)))
-    else:
-        await ctx.send("{} entrou pra porrada".format(ctx.author.name))
+    await QUEUE.add_player(ctx)
 
 
 @bot.command(name="sair")
 # @is_channel("eventos")
 async def leave_queue(ctx):
-    if QUEUE.remove_player(ctx):
-        await ctx.send(ctx.author.name + " removido da fila")
-    else:
-        await ctx.send(f'Jogador nao consta na fila')
+    await QUEUE.remove_player(ctx)
 
 
 @bot.command(name="pular")
 # @is_channel("eventos")
 async def skip_turn(ctx):
-    if QUEUE.skip_turn(ctx):
-        await ctx.send(ctx.author.name + " foi para o fim da fila")
-    else:
-        await ctx.send(f'Nao foi possivel completar sua chamada, verifique o numero discado ou consulte informacoes DDD')
+    await QUEUE.skip_turn(ctx)
 
 
 @bot.command(name="ggez")
 # @is_channel("eventos")
 async def ggez(ctx):
-    QUEUE.resolve_match(ctx)
+    await QUEUE.resolve_match(ctx)
 
 
 @bot.command(name="fila")
